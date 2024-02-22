4194d81433ef5602bde2c164fe483c004f87d870
15585022.clot@fje.edu <15585022.clot@fje.edu>
2024-02-22 15:18:14 +0000

    Prova Validació

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
     <div class="container mt-5 d-flex align-items-center justify-content-center">
         <div class="welcome text-center p-4 rounded bg-light">
-            <span>Projecte GP1 - ASIX2 - 2024</span><br>
+            <span>Projecte GP1 - ASIX2 - Marc Olle Pastor gp1f4a15 20240222</span><br>
             <button class="btn btn-primary btn-lg" onclick="window.location.href='page_installation.php'">Accedeix!</button><br>
             <span class="text-gray">Grup 06</span>
         </div>
